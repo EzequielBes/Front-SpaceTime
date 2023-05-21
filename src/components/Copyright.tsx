dd7d2cc44445022ea303a22c7265075114cdc79d
ezequielbes <ezequieltdbeserra@gmail.com>
2023-05-21 19:53:19 -0300

    Completed project design, added features and routes

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com no nwl{' '}
+      <a
+        target="_blank"
+        rel="norefe"
+        className="underline hover:text-gray-100"
+        href=""
+      >
+        RocketSeat
+      </a>
+    </div>
+  )
+}
